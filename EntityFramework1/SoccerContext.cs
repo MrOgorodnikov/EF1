e9321b51f6cd430dd9f722e52b4271ab2bd00299
MrOgorodnikov <v.ogo@yandex.ru>
2016-09-27 14:59:43 +0300

    Добавьте файлы проекта.

---
+++ EntityFramework1/SoccerContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework1
+{
+    class SoccerContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+    }
+}
