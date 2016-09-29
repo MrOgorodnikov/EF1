d82924fc47bc9668558ed4d26c415b21fdc410d7
MrOgorodnikov <v.ogo@yandex.ru>
2016-09-29 11:50:30 +0300

    Add Change button

--- EntityFramework1/Form1.Designer.cs
+++ EntityFramework1/Form1.Designer.cs
@@ -43,7 +43,9 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(918, 150);
@@ -67,6 +69,7 @@
             this.Change.TabIndex = 2;
             this.Change.Text = "Change";
             this.Change.UseVisualStyleBackColor = true;
+            this.Change.Click += new System.EventHandler(this.Change_Click);
             // 
             // Delete
             // 
--- EntityFramework1/Form1.cs
+++ EntityFramework1/Form1.cs
@@ -28,17 +28,46 @@ namespace EntityFramework1
         {
             Player player = new Player();
             AddPlayer plForm = new AddPlayer();
-            DialogResult result = plForm.ShowDialog(this);
 
-            if (result == DialogResult.Cancel)
+            db.Players.Add(CreatePlayer(player, plForm));
+            db.SaveChanges();
+        }
+
+        private void Change_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount < 0)
+                return;
+            //var index = dataGridView1.SelectedRows[0].Index;
+            int playerId = 0;
+            bool getId = Int32.TryParse(dataGridView1[0, dataGridView1.SelectedRows[0].Index].Value.ToString(), out playerId);
+            if (!getId)
                 return;
 
-            player.Name = plForm.NameTextBox.Text;
-            player.Age = (int)plForm.AgeNumericUpDown.Value;
-            player.Position = plForm.PositionComboBox.SelectedItem.ToString();
+            var player = db.Players.Find(playerId);
 
-            db.Players.Add(player);
+            AddPlayer changeForm = new AddPlayer();
+            changeForm.NameTextBox.Text = player.Name;
+            changeForm.AgeNumericUpDown.Value = player.Age;
+            changeForm.PositionComboBox.SelectedItem = player.Position;
+            CreatePlayer(player, changeForm);
+
             db.SaveChanges();
+
+            dataGridView1.Refresh();
+        }
+
+        private Player CreatePlayer(Player player, AddPlayer form)
+        {
+            DialogResult result = form.ShowDialog(this);
+
+            if (result == DialogResult.Cancel)
+                return null;
+
+            player.Name = form.NameTextBox.Text;
+            player.Age = (int)form.AgeNumericUpDown.Value;
+            player.Position = form.PositionComboBox.SelectedItem.ToString();
+
+            return player;
         }
     }
 }
